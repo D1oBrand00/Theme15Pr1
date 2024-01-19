9a24bafac5918b45dba98fe560e65d983485c981
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-01-19 14:34:00 +0200

    Add files via upload

---
+++ Theme15Pr1/Theme15Pr1/MyClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme15Pr1
+{
+    /// <summary>
+    /// Нестатический класс описывающий класс
+    /// </summary>
+    class MyClass
+    {
+        /// <summary>
+        /// Поле Ф.И.О школьника
+        /// </summary>
+        string name;
+        /// <summary>
+        /// Поле пола школьника
+        /// </summary>
+        string gender;
+        /// <summary>
+        /// Поле года рождения школьника
+        /// </summary>
+        int birthyear;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = (value == "м" || value == "ж") ? value : "Такого пола нет"; }
+        }
+        public int Birthyear
+        {
+            get { return birthyear; }
+            set { birthyear = value; }
+        }
+        public MyClass(string name, string gender, int birthyear)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.birthyear = birthyear;
+        }
+        public void Count(MyClass[]pupil)
+        {
+            int countmale = 0;
+            int countfemale = 0;
+            foreach (var p in pupil)
+            {
+                if(p.Gender == "м")
+                {
+                    countmale++;
+                }    
+                else if (p.Gender == "ж")
+                {
+                    countfemale++;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Theme15Pr1/Theme15Pr1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme15Pr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            MyClass[] pupil = new MyClass[n];
+            for (int i = 0; i < pupil.Length; i++)
+            {
+                Console.Write("Ввелие ФИО школьника:");
+                string name = Console.ReadLine();
+                Console.Write("Введите пол школьника:");
+                string gender = Console.ReadLine();
+                Console.Write("Введите год рождения школьника:");
+                int birthyear = int.Parse(Console.ReadLine());
+                pupil[i] = new MyClass(name, gender, birthyear);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
