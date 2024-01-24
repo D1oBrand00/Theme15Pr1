8a3253cb73a90c67fa2432c82afe097cd2c330ba
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-01-24 11:03:35 +0200

    Update Program.cs

--- Theme15Pr1/Theme15Pr1/Program.cs
+++ Theme15Pr1/Theme15Pr1/Program.cs
@@ -10,9 +10,51 @@ namespace Theme15Pr1
     {
         static void Main(string[] args)
         {
-            int n = 3;
-            Schoolboy[] students = new Schoolboy[n];
-            for (int i = 0; i < students.Length; i++)
+            //1
+            //Console.Write("Кол-во школьников - ");
+            //int n = int.Parse(Console.ReadLine());
+            //Schoolboy[] students = new Schoolboy[n];
+            //for (int i = 0; i < students.Length; i++)
+            //{
+            //    Console.Write("Введите фамилию: ");
+            //    string surname = Console.ReadLine();
+            //    Console.Write("Ввелие имя: ");
+            //    string name = Console.ReadLine();
+            //    Console.Write("Ввелие отчество: ");
+            //    string patronymic = Console.ReadLine();
+            //    Console.Write("Введите пол(м/ж): ");
+            //    string gender = Console.ReadLine();
+            //    Console.Write("Введите год рождения: ");
+            //    int birthyear = int.Parse(Console.ReadLine());
+            //    students[i] = new Schoolboy(surname, name, patronymic, gender, birthyear);
+            //    Console.WriteLine();
+
+            //}
+            //Console.WriteLine("Мальчики: \n");
+            //foreach (var schoolboy in students)
+            //{
+            //    if (schoolboy.Gender == "м" || schoolboy.Gender == "М")
+            //    {
+            //        schoolboy.Print();
+            //        Console.WriteLine();
+            //    }
+            //}
+            //Console.WriteLine("Девочки: \n");
+            //foreach (var schoolboy in students)
+            //{
+            //    if (schoolboy.Gender == "ж" || schoolboy.Gender == "Ж")
+            //    {
+            //        schoolboy.Print();
+            //        Console.WriteLine();
+            //    }
+            //}
+            //Console.WriteLine($"Кол-во мальчиков - {Schoolboy.Countmale}\nКол-во девочек - {Schoolboy.Countfemale}");
+
+            //2
+            Console.Write("Кол-во спорстменов - ");
+            int n = int.Parse(Console.ReadLine());
+            Sportsmen[] sportsmens = new Sportsmen[n];
+            for (int i = 0; i < sportsmens.Length; i++)
             {
                 Console.Write("Введите фамилию: ");
                 string surname = Console.ReadLine();
@@ -20,33 +62,23 @@ namespace Theme15Pr1
                 string name = Console.ReadLine();
                 Console.Write("Ввелие отчество: ");
                 string patronymic = Console.ReadLine();
-                Console.Write("Введите пол: ");
-                string gender = Console.ReadLine();
-                Console.Write("Введите год рождения: ");
-                int birthyear = int.Parse(Console.ReadLine());
-                students[i] = new Schoolboy(surname, name, patronymic, gender, birthyear);
+                Console.Write("Введите рост(в см): ");
+                int weight = int.Parse(Console.ReadLine());
+                Console.Write("Введите вес(в кг): ");
+                int height = int.Parse(Console.ReadLine());
+                sportsmens[i] = new Sportsmen(surname, name, patronymic, weight, height);
                 Console.WriteLine();
 
             }
-            Console.WriteLine("Мальчики: \n");
-            foreach (var schoolboy in students)
+            Console.Write("Спортсмены, чей вес превышает 70 кг: \n");
+            foreach (var sportmens in sportsmens)
             {
-                if (schoolboy.Gender == "м" || schoolboy.Gender == "М")
-                {
-                    schoolboy.Print();
-                    Console.WriteLine();
+                if (sportmens.Weight > 70) 
+                { sportmens.Output(); 
+                  Console.WriteLine(); 
                 }
             }
-            Console.WriteLine("Девочки: \n");
-            foreach (var schoolboy in students)
-            {
-                if (schoolboy.Gender == "ж" || schoolboy.Gender == "Ж")
-                {
-                    schoolboy.Print();
-                    Console.WriteLine();
-                }
-            }
-            Console.WriteLine($"Кол-во мальчиков - {Schoolboy.Countmale}\nКол-во девочек - {Schoolboy.Countfemale}");
+            Console.WriteLine($"Кол-во спортсменов, чей вес превышает 70 кг - {Sportsmen.Count}");
 
 
             Console.ReadKey();
